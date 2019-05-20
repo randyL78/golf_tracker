6fa47a70fc52a5dfe3646ec3710100e62e339e7b
Randy's Mac <randydavidl78@gmail.com>
2019-05-20 08:07:04 -0400

    Add skeleton React app to client

---
+++ client/src/components/App.jsx
@@ -0,0 +1,12 @@
+// dependencies
+import React from 'react';
+
+// components
+import Home from './Home';
+
+const App = props =>
+  <div>
+    <Home />
+  </div>;
+
+export default App;
---
+++ client/webpack.config.babel.js
@@ -0,0 +1,9 @@
+import module from './webpack/configuration/module';
+import plugins from './webpack/configuration/plugins';
+import resolve from './webpack/configuration/resolve';
+
+export {
+  module,
+  plugins,
+  resolve
+};
